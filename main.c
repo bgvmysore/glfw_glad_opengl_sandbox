922109c912ffdca479f136a3680bc7e78121aa30
Bhargav R <bgvmysore@gmail.com>
2022-10-30 16:00:17 +0530

    Implementation of Model View Projection Matrix
    Added 3 uniforms of type matrix4, the same was created in vertex shader also.
    Finally create a simple functionality where the object will trach the mouse positon if the
    left mouse button is held, this functionality makes use of model view matrix and glfw mouse input functions.

--- main.c
+++ main.c
@@ -10,6 +10,10 @@
 
 #include "shader_loader.h"
 
+
+#define WINDOW_WIDTH  640
+#define WINDOW_HEIGHT 480
+
 #define NUM_VERTICES 4
 #define NUM_INDICES  6
 
@@ -46,7 +50,7 @@ int main(void) {
 	if (!glfwInit()) return -1;
 
 	/* Create a Glfw Window */
-	window = glfwCreateWindow(600, 600, "Triangle", NULL, NULL);
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Sandbox", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
@@ -110,6 +114,7 @@ int main(void) {
 	}
 
 	glGenTextures(1, &texture_id); // Generate 1 texter buffer and store its id in 'texture_id'
+	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture_id); // Bind the given texture
 
 	// Set Texture Buffer Parameters
@@ -121,10 +126,10 @@ int main(void) {
 	// Set texture image buffer on GPU and load pixel data
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, tex_image_width, tex_image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex_image_buffer);
 
+	glActiveTexture(0);
 	glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture
 
 	stbi_image_free(tex_image_buffer); // Free the image buffer as it is already in GPU
-
 
 	/* GL Shaders*/
 	/* Load shader sources on to thier respective char buffers from file path specified */
@@ -137,8 +142,38 @@ int main(void) {
 
 	glUseProgram(shader_program_id); // Use the specified shader program using its id
 
+	/* Get Uniform Locations for required uniforms */
+	GLint loc_proj_mat, loc_view_mat, loc_mod_mat, loc_tex_sampler_slot;
+	loc_proj_mat = glGetUniformLocation(shader_program_id, "proj_mat");
+	loc_view_mat = glGetUniformLocation(shader_program_id, "view_mat");
+	loc_mod_mat = glGetUniformLocation(shader_program_id, "mod_mat");
+
+	loc_tex_sampler_slot = glGetUniformLocation(shader_program_id, "tex_sampler_slot");
+
+	/* Creating Projection Matrix Ortho (2D) */
+	mat4 projection_mat = GLM_MAT4_IDENTITY_INIT;
+	GLfloat window_aspect_ratio = (GLfloat) WINDOW_WIDTH/ (GLfloat) WINDOW_HEIGHT;
+	glm_ortho(-1 * window_aspect_ratio, window_aspect_ratio, -1, 1, -1, 1, projection_mat);
+
+	// Setting Uniform, Note this should be done after binding shader program
+	glUniformMatrix4fv(loc_proj_mat, 1, GL_FALSE, (GLfloat*)projection_mat);
+
+	/* Creating View or Camera Matrix */
+	mat4 camera_mat = GLM_MAT4_IDENTITY_INIT;
+	glm_translate(camera_mat, (vec3){0.125f, 0.3f, 0.0f});
+	glUniformMatrix4fv(loc_view_mat, 1, GL_FALSE, (GLfloat*)camera_mat);
+
+	/* Creating Model Matrix */
+	mat4 model_mat = GLM_MAT4_IDENTITY_INIT;
+	glUniformMatrix4fv(loc_mod_mat, 1, GL_FALSE, (GLfloat*)model_mat);
+
 	/* Main Program Loop */
+	double mouse_x, mouse_y;
+	float trans_vec2[2] = {0};
+	float time_0 = glfwGetTime(), time_1;
+	float fps_frams = 0;
 	while(!glfwWindowShouldClose(window)) {
+
 		glClearColor(0.3f, 0.33f, 0.37f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
@@ -149,14 +184,42 @@ int main(void) {
 		glActiveTexture(GL_TEXTURE0); // Bind required texture slot
 		glBindTexture(GL_TEXTURE_2D, texture_id); // Bind texture given by id
 
-		// Get loaction of the sampler slot and give it slot info
-		// in our case we have GL_TEXTURE0 so we give that uniform a value of 0
-		glUniform1i(glGetUniformLocation(shader_program_id, "tex_sampler_slot"), 0);
+		// Give texture slot info, in our case we have GL_TEXTURE0 so we give that uniform a value of 0
+		glUniform1i(loc_tex_sampler_slot, 0);
+
+		// Set value for camera matrix uniform
+		glUniformMatrix4fv(loc_view_mat, 1, GL_FALSE, (GLfloat*)camera_mat);
 
 		glDrawElements(GL_TRIANGLES, NUM_INDICES, GL_UNSIGNED_BYTE, 0); // Using glDrawElements instead glDrawArrays
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		/* FPS Calculation */
+		fps_frams += 1.0f;
+		time_1 = glfwGetTime();
+		if (time_1 - time_0 > 5) {
+			printf("FPS: %f \n", fps_frams/5);
+			fps_frams = 0.0f;
+			time_0 = time_1;
+
+			printf("Mouse Norm: ( %f , %f ) \n", trans_vec2[0], trans_vec2[1]);
+		}
+
+		/* Object Track Mouse */
+		if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS) {
+			glfwGetCursorPos(window, &mouse_x, &mouse_y);
+			if(mouse_x >= WINDOW_WIDTH) mouse_x = (double) WINDOW_WIDTH;
+			if(mouse_y >= WINDOW_HEIGHT) mouse_y = (double) WINDOW_HEIGHT;
+			if(mouse_x <= 0) mouse_x = (double) 0;
+			if(mouse_y <= 0) mouse_y = (double) 0;
+
+			trans_vec2[0] = 2.0f * mouse_x / (float) WINDOW_WIDTH -  1.0f;
+			trans_vec2[1] = -2.0f * mouse_y / (float) WINDOW_HEIGHT + 1.0f;
+
+			glm_mat4_copy(GLM_MAT4_IDENTITY, camera_mat);
+			glm_translate(camera_mat, (vec3){trans_vec2[0], trans_vec2[1], 0.0f});
+		}
 	}
 
 	/* Clean Up */
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -8,8 +8,12 @@ layout (location = 2) in vec2 att_tex;
 out vec3 vert_color;
 out vec2 frg_tex_coord;
 
+uniform mat4 proj_mat;
+uniform mat4 view_mat;
+uniform mat4 mod_mat;
+
 void main() {
-	gl_Position = vec4(att_pos, 1.0);
+	gl_Position = proj_mat * view_mat * mod_mat * vec4(att_pos, 1.0);
 	vert_color = att_color;
 	frg_tex_coord = att_tex;
 }
